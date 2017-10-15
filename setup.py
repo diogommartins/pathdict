fbe7bb24959827f1b3b0ba9adbd959f737382915
diogommartins <magalhaesmartins@icloud.com>
2017-10-15 17:49:53 -0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 
 setup(name='pathdict',
       version=VERSION,
       packages=find_packages(exclude=['*test*']),
       url='https://github.com/diogommartins/pathdict',
-      author='Diogo Magalhães Martins',
+      author='Diogo Magalhaes Martins',
       author_email='magalhaesmartins@icloud.com',
       keywords='collection dictionary dict path dotted',
       long_description_markdown_filename='README.md')
